a92bba1b17d32392bde038049d009e42d36f0bbf
Gabriel Francke <s27151@pjwstk.edu.pl>
2022-12-17 13:05:42 +0100

    brunch update

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1, b = 10
+    cout<<"World Hello";
+    cout<<"Perception";
+    return 0;
+
+int multiply(int a,int b)
+{
+    
+    return a+b;
+
+}
+}
